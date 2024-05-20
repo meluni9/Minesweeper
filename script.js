4be9e111bf4acbbfbafb5abbf47229ece6f5df84
ani163 <melnika243@gmail.com>
2024-05-20 16:06:29 +0300

    minor fixes

--- script.js
+++ script.js
@@ -16,18 +16,27 @@ import { startTimer, stopTimer } from "./timer.js";
 const BOARD_SIZE = 10;
 const NUMBER_OF_MINES = 10;
 
-const boardElement = document.querySelector(".board");
-const minesLeftText = document.querySelector("[data-mine-count]");
+const boardElement = document.querySelector("#board");
+const minesLeftText = document.querySelector("#mines-left");
 const messageText = document.querySelector(".subtext");
-const timerElement = document.querySelector("[data-timer]");
+const timerElement = document.querySelector("#timer");
+const resetButton = document.querySelector("#reset-button");
 
 const init = () => {
+    resetBoard();
     const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
     setupBoard(board);
     boardElement.style.setProperty("--size", BOARD_SIZE);
-    minesLeftText.textContent = NUMBER_OF_MINES;
+    timerElement.textContent = "0";
     startTimer(timerElement);
-};
+}
+
+const resetBoard = () => {
+    boardElement.innerHTML = "";
+    messageText.textContent = " ";
+    minesLeftText.textContent = NUMBER_OF_MINES.toString();
+    stopTimer();
+}
 
 const setupBoard = (board) => {
     for (let row of board) {
@@ -44,14 +53,17 @@ const setupBoard = (board) => {
             });
         }
     }
-};
+}
 
 const listMinesLeft = (board) => {
     const markedTilesCount = board.reduce((count, row) => {
-        return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length;
+        return (
+            count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
+        );
     }, 0);
-    minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount;
-};
+
+    minesLeftText.textContent = (NUMBER_OF_MINES - markedTilesCount).toString();
+}
 
 const checkGameEnd = (board) => {
     const win = checkWin(board);
@@ -59,8 +71,7 @@ const checkGameEnd = (board) => {
 
     if (win || lose) {
         stopTimer();
-        boardElement.addEventListener("click", stopProp, { capture: true });
-        boardElement.addEventListener("contextmenu", stopProp, { capture: true });
+        disableBoardInteraction();
     }
 
     if (win) {
@@ -68,20 +79,39 @@ const checkGameEnd = (board) => {
     }
     if (lose) {
         messageText.textContent = "You lose!";
-        for (let row of board) {
-            for (let tile of row) {
-                if (tile.status === TILE_STATUSES.MARKED) {
-                    markTile(tile);
-                }
-                if (tile.mine) {
-                    revealTile(board, tile);
-                }
+        revealAllMines(board);
+    }
+}
+
+const revealAllMines = (board) => {
+    for (let row of board) {
+        for (let tile of row) {
+            if (tile.status === TILE_STATUSES.MARKED) {
+                markTile(tile);
+            }
+            if (tile.mine) {
+                revealTile(board, tile);
             }
         }
     }
-};
+}
+
+const disableBoardInteraction = () => {
+    boardElement.addEventListener("click", stopProp, { capture: true });
+    boardElement.addEventListener("contextmenu", stopProp, { capture: true });
+}
+
+const enableBoardInteraction = () => {
+    boardElement.removeEventListener("click", stopProp, { capture: true });
+    boardElement.removeEventListener("contextmenu", stopProp, { capture: true });
+}
 
 const stopProp = (e) => e.stopImmediatePropagation();
+
+resetButton.addEventListener("click", () => {
+    enableBoardInteraction();
+    init();
+});
 
 document.addEventListener("DOMContentLoaded", () => {
     init();
